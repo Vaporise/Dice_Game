222fc0bf9914d6495f67344c93230c080dc23a09
Vaporise <draxdragonfire@googlemail.com>
2025-09-29 14:18:04 +0900

    Fixed the while loop and upper/lower case Y/N issue. Game complete

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Dice_Game
 Coding practice to create a dice game.
+
+To run Dice Game you need to have python installed.
+
+you can run it using *python3 main.py*
--- main.py
+++ main.py
@@ -8,17 +8,17 @@ def main():
 
         print("Roll the dice? (y/n):") # First the choice needs to be made
         choice = input()
-        if choice == "Y" or "y": # If the choice yes is chosen then there is a loop or while as long as y is chosen.
+        if choice.lower() == "y": # if the choice of y is taken regardless of upper or lower case.
             int1 = random.randint(1, 6)
             int2 = random.randint(1, 6)
             print(f"({int1},{int2})")
         
-        elif choice == "n" or "N": #If no is chosen the game is over so print an message and exit
+        elif choice.lower() == "n": #If no is chosen regardless of upper or lower case the game is over so print an message and exit
             looping = False #Exit the while loop
-        print("Thanks for playing!")
-        exit
-    else: #If the choice is not yes or no then it is an invalid choice.
-        print("Invalid Choice!")
+            print("Thanks for playing!")
+            exit
+        else: #If the choice is not yes or no then it is an invalid choice.
+            print("Invalid Choice!")
 
 if __name__ == "__main__":
     main()
